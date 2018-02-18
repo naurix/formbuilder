1a2bb7fdcd3fa685109e477601e098355f42e1a5
Arthur Ibik <arthuribik@gmail.com>
2018-02-18 03:49:50 +0100

    Complete frist release

---
+++ js/app.js
@@ -0,0 +1,571 @@
+// Drag and drop
+$( "#sortable" ).sortable({
+	revert: true,
+	handel: '.cb_move_btn',
+	cancel: '.no-drag',
+});
+// $( "#sortable" ).disableSelection();
+
+// Elements
+var __col_btn = $('.col_btn');
+var __mr_btn = $('.main_row_btn');
+var __mr_box = $('.main_row_append_box');
+var __mr_btn_mini = $('.main_row_minimize');
+var __generate_btn = $('.generate_btn');
+var __generate_box = $('.form_generated_box');
+var __html_btn = $('.html_btn');
+var __code_box = $('.code_box');
+var __doc = $(document);
+
+
+// Events
+function make_column(e){
+	e.preventDefault();
+	var d = $(this).data('col-btn');
+	console.log(d);
+}
+
+function add_main_row(e){
+	e.preventDefault();
+
+	__mr_box.append(main_row);
+	// console.log(main_row)
+}
+
+function add_inner_row(e){
+	e.preventDefault();
+
+	var par = $(this).parents('.cb_display_box:first');
+	var box = par.find('.inner_row_append_box:first');
+	box.append(inner_row);
+}
+
+function add_column(){
+	var par = $(this).parents('.inner_col__box:first');
+	var n = $(this).val();
+	var col_d = '';
+	var col = '';
+	
+	if(parseInt(n) == 1) {col = 'col-sm-12'; }
+	if(parseInt(n) == 2) {col = 'col-sm-6';}
+	if(parseInt(n) == 3) {col = 'col-sm-4';}
+	if(parseInt(n) == 4) {col = 'col-sm-3';}
+
+	for(var i = 0; i < n; i++){
+		col_d += '<div class="'+col+'">'+form_select_bar+'</div>';
+	}
+
+	var box = par.find('.form_row_box');
+	box.html(col_d);
+}
+
+// Make Field
+function on_select_field(){
+	var txt = $(this).val();
+	var par = $(this).parent('.field_box');
+	var box = par.find('.form_field_box');
+
+	$(this).hide();
+	box.text('Here')
+
+	switch(txt){
+		case 'input':
+			make_input_field(box);
+			break;
+
+		case 'textarea':
+			make_textarea_field(box);
+			break;
+
+		case 'select':
+			make_select_field(box);
+			break;
+
+		case 'checkbox':
+			make_checkbox_field(box);
+			break;
+
+		case 'radio':
+			make_radio_field(box);
+			break;
+
+		default:
+			console.log('Not added');
+	}
+	// console.log(txt);
+}
+
+// Create Input
+function make_input_field(el){
+	var id = make_id(6);
+	var input = '<div class="cnt_input_box form-group">';
+		input += '<label for="'+id+'" data-field-type="input" class="in_field">';
+		input += '<span class="field_title" contenteditable>Input Field</span>';
+		input += in_field_edit;
+		input += '</label>';
+		input += '<input type="text" name="'+id+'" class="form-control" placeholder="">';
+		input += '</div>';
+	el.html(input);
+}
+
+// Create Textarea
+function make_textarea_field(el){
+	var id = make_id(6);
+	var input = '<div class="cnt_input_box form-group">';
+		input += '<label for="'+id+'" data-field-type="text" class="in_field">';
+		input += '<span class="field_title" contenteditable>Note Field</span>';
+		input += in_field_edit;
+		input += '</label>';
+		input += '<textarea type="text" name="'+id+'" class="form-control" placeholder=""></textarea>';
+		input += '</div>';
+	el.html(input);
+}
+
+// Create Select
+function make_select_field(el){
+	var id = make_id(6);
+	var input = '<div class="cnt_input_box form-group">';
+		input += '<label for="'+id+'" data-field-type="select" class="in_field">';
+		input += '<span class="field_title" contenteditable>Select Field</span>';
+		input += in_field_edit;
+		input += '</label>';
+		input += add_option_temp('select');
+		input += '<select name="'+id+'" class="form-control hide_box">';
+		input += '<option disabled="" selected="">-- Select Option --</option>';
+		input += '</select>';
+		input += '</div>';
+	el.html(input);
+}
+
+// Create Checkbox
+function make_checkbox_field(el){
+	var id = make_id(6);
+	var input = '<div class="cnt_input_box form-group">';
+		input += '<label for="'+id+'" data-field-type="checkbox" class="in_field">';
+		input += '<span class="field_title" contenteditable>Checkbox Field</span>';
+		input += in_field_edit;
+		input += '</label>';
+		input +=  add_option_temp('checkbox');
+		input += '</div>';
+	el.html(input);
+}
+
+// Create Radio
+function make_radio_field(el){
+	var id = make_id(6);
+	var input = '<div class="cnt_input_box form-group">';
+		input += '<label for="'+id+'" data-field-type="radio" class="in_field">';
+		input += '<span class="field_title" contenteditable>Radio Field</span>';
+		input += in_field_edit;
+		input += '</label>';
+		input +=  add_option_temp('radio');
+		input += '</div>';
+	el.html(input);
+}
+
+// Select option
+function add_options(){
+	var par = $(this).parents('.cnt_input_box');
+	var type = $(this).attr('data-option-text');
+	var s_ul = par.find('.select_list');
+	var s_box = par.find('.select_add_list');
+	var inp = s_box.find('input:first');
+	var sel = par.find('select:first');
+	var icon = '<i class="fa fa-close"></i>';
+
+	if(type === 'select'){
+		if(inp.val() !== ''){
+			var el = '<span class="delete" data-rem-text="select">'+icon+'</span></li>';
+			s_ul.append('<li data-select-val="'+inp.val()+'">'+ inp.val() +el);
+			sel.append('<option>'+inp.val()+'</option>');
+			inp.val('');
+		}
+	}
+
+	if(type === 'checkbox'){
+		var label = par.find('label:first');
+		var type = label.data('field-type');
+		var txt = label.text();
+
+		var el = '<div class="checkbox">';
+			el += '<label>';
+			el += '<input type="checkbox" name="'+slugify(txt.trim())+'" value="'+inp.val()+'">'+inp.val();
+			el += '<span class="delete" data-rem-text="checkbox">'+icon+'</span></li>';
+			el += '</label>';
+			el += '</div>';
+
+		s_ul.append(el);
+		inp.val('');
+	}
+
+	if(type === 'radio'){
+		var label = par.find('label:first');
+		var type = label.data('field-type');
+		var txt = label.text();
+
+		var el = '<div class="radio">';
+			el += '<label>';
+			el += '<input type="radio" name="'+slugify(txt.trim())+'" value="'+inp.val()+'">'+inp.val();
+			el += '<span class="delete" data-rem-text="radio">'+icon+'</span></li>';
+			el += '</label>';
+			el += '</div>';
+
+		s_ul.append(el);
+		inp.val('');
+	}
+}
+
+// Edit field field 
+function add_field_title(){
+	var par = $(this).parents('.cnt_input_box');
+	var label = par.find('label:first');
+	var type = label.data('field-type');
+	var txt = label.text();
+
+	if(type === 'input'){
+		par.find('input:first').attr('name', slugify(txt));
+		par.find('input:first').attr('placeholder', txt.trim());
+	}
+
+	// 
+	if(type === 'text'){
+		par.find('textarea:first').attr('name', slugify(txt));
+		par.find('textarea:first').attr('placeholder', txt.trim());
+	}
+
+	if(type === 'select'){
+		var sel = par.find('select:first');
+		sel.attr('name', slugify(txt));
+		sel.attr('placeholder', txt.trim());
+		sel.find('option:first').text('-- '+txt.trim()+' --');
+	}
+
+	if(type === 'checkbox'){
+		var cbox = par.find('.checkbox');
+		var inp = cbox.find('input');
+		inp.attr('name', slugify(txt.trim() ));
+	}
+
+	if(type === 'radio'){
+		console.log('Radio here');
+		var cbox = par.find('.radio');
+		var inp = cbox.find('input');
+		inp.attr('name', slugify(txt.trim() ));
+	}
+}
+
+function update_field_obj(){
+	//
+}
+
+// Generate form data
+function generate_form_data(){
+	var c = __mr_box.clone(true, true);
+	var mainbox = c.find('.cb_display_box');
+	var removables = c.find('.removable');
+	
+	var btn = __html_btn;
+	var btn_txt = btn.attr('data-code-btn');
+
+	// Remove handlers
+	c.find('.removable, .delete, .remove_btn').remove();
+	c.removeClass('cb_display_box, no-drag');
+
+	__generate_box.html('');
+	mainbox.each(function(i, v){		
+		var heading = $(this).find('h2:first');
+		var desc = $(this).find('.add_desc_box:first');
+		var desc = desc.text().trim();
+
+		var box = '<div class="main-group-box">';
+		box += '<h2 class="group_title">'+heading.text()+'</h2>';
+
+		if(desc !== '-- Description text here' && desc !== ''){
+			box += '<p>'+desc+'</p>';
+		}
+
+		var fbox = $(this).find('.form_row_box');
+		var el = '';
+		fbox.each(function(i, v){
+			el += '<div class="row">';
+
+			var cols = $(this).children();
+			for(var j = 0; j < cols.length; j++){
+				var field = $(this).find('.cnt_input_box');
+				var c = cols[j].getAttribute('class');
+				el += '<div class="'+c+'">';
+				el += field[j].outerHTML;
+				el += '</div>';
+			}
+			el += '</div>';
+		});
+
+		box += el;
+		box += '</div>';
+
+		__generate_box.append(box);
+	});
+
+	__mr_box.hide();
+	__generate_box.show();
+
+	// __generate_box.html(c);
+	__mr_btn.hide();
+	btn.attr('data-code-btn', 'editor');
+	btn.html('<i class="fa fa-angle-double-left"></i> Back');
+}
+
+// Make HTML View
+function switch_view(){
+	var c = __mr_box.clone(true, true);
+	var mainbox = c.find('.cb_display_box');
+
+	var btn = $(this);
+	var btn_txt = btn.attr('data-code-btn');
+
+	// Remove handlers
+	if(btn_txt === 'code'){
+		mainbox.find('.removable').remove();
+
+		__code_box.val('');
+		mainbox.each(function(i, v){
+			var heading = $(this).find('h2:first');
+			var desc = $(this).find('.add_desc_box:first');
+			var desc = desc.text().trim();
+
+			var box = '<div class="main-group-box">';
+			box += '<h2 class="group_title">'+heading.text()+'</h2>';
+
+			if(desc !== '-- Description text here' && desc !== ''){
+				box += '<p>'+desc+'</p>';
+			}
+
+			var fbox = $(this).find('.form_row_box');
+			var el = '';
+			fbox.each(function(i, v){
+				el += '<div class="row">';
+
+				var cols = $(this).children();
+				for(var j = 0; j < cols.length; j++){
+					var field = $(this).find('.cnt_input_box');
+					var c = cols[j].getAttribute('class');
+					el += '<div class="'+c+'">';
+					el += field[j].outerHTML;
+					el += '</div>';
+				}
+				el += '</div>';
+			});
+
+			box += el;
+			box += '</div>';
+
+			var txt = __code_box.val();
+			__code_box.val(txt + box);
+		});
+
+		__mr_box.hide();
+		__generate_box.hide();
+		__code_box.show();
+
+		btn.attr('data-code-btn', 'editor');
+		btn.html('<i class="fa fa-angle-double-left"></i> Back');
+	}
+
+	if(btn_txt === 'editor'){
+		__code_box.hide();
+		__generate_box.hide();
+		__mr_box.show();
+		__mr_btn.show();
+
+		btn.attr('data-code-btn', 'code');
+		btn.html('View HTML <i class="fa fa-code"></i>')
+	}
+}
+
+function heading_text(){
+	var par = $(this).parent('.desc_box');
+	var box = par.find('.add_desc_box');
+	$(this).hide();
+	box.show();
+}
+
+function remove_action(){
+	var btn = $(this);
+	var txt = btn.attr('data-remove-txt');
+
+	// alert 
+
+	if(txt === 'main'){
+		var box = $(this).parents('.cb_layer_box:first');
+		box.remove();
+	}
+
+	if(txt === 'inner_row'){
+		var box = $(this).parents('.inner_col__box:first');
+		box.remove();
+	}
+
+	if(txt === 'field'){
+		var par = btn.parents('.field_box:first');
+		var sel = par.find('.form_field_select:first');
+		var div = par.find('div:first');
+
+		// Alert first
+
+		div.html('');
+		var op = sel.find('option')[0];
+		sel.val(op.innerText);
+		sel.show();
+	}
+
+	if(txt === 'heading'){
+		var par = btn.parents('.desc_box');
+		var btn = par.find('.add_desc_btn');
+		var box = par.find('.add_desc_box');
+
+		box.find('span:first').text('-- Description text here');
+
+		box.hide();
+		btn.show();
+	}
+}
+
+function minimize_action(){
+	var btn = $(this);
+	var txt = btn.attr('data-minimize-txt');
+
+	if(txt === 'main'){
+		var par = btn.parents('.cb_layer_box:first');
+		var min_tb = par.find('.on_minimize:first');
+		var min_txt = par.find('.group_title:first');
+		var side_bar = par.find('.side_move_bar:first');
+
+		var box = par.find('.minimize_box:first');
+		if(box.is(':visible')){
+			par.addClass('minimize_cover');
+			box.hide();
+			btn.html('<i class="fa fa-plus"></i>');
+			min_tb.html('<h2 class="group_title">'+min_txt.text()+'</h2>');
+
+			side_bar.removeClass('cb_move_btn');
+		}else{
+			min_tb.html('');
+			par.removeClass('minimize_cover');
+			box.show();
+			btn.html('<i class="fa fa-minus"></i>');
+			side_bar.addClass('cb_move_btn');
+		}
+	}
+
+	if(txt === 'inner_row'){
+		var par = btn.parents('.inner_col__box:first');
+		var box = par.find('.form_row_box:first');
+		if(box.is(':visible')){
+			box.hide();
+			btn.html('<i class="fa fa-plus"></i>');
+		}else{
+			box.show();
+			btn.html('<i class="fa fa-minus"></i>');
+		}
+	}
+}
+
+function copy_action(){
+	var btn = $(this);
+	var txt = btn.attr('data-copy-txt');
+
+	if(txt === 'main'){
+		var row = btn.parents('.main_row_append_box:first');
+		var box = btn.parents('.cb_layer_box:first');
+
+		var c = box.clone(true, true);
+		box.after(c);
+	}
+
+	if(txt === 'inner_row'){
+		var row = btn.parents('.inner_row_append_box:first');
+		var box = btn.parents('.inner_col__box:first');
+
+		var c = box.clone(true, true);
+		box.after(c);
+	}
+}
+
+// Delete Option 
+function remove_option(){
+	var txt = $(this).attr('data-rem-text');
+	var box;
+
+	// alert
+
+	if(txt === 'checkbox' || txt === 'radio'){
+		box = $(this).parents('.'+txt+':first');
+		box.remove();
+	}
+
+	if(txt === 'select'){
+		var par = $(this).parents('.cnt_input_box:first');
+		var li = $(this).parent('li');
+		var op_txt = li.attr('data-select-val');
+		var sel = par.find('select');
+
+		var ops = sel.find('option');
+
+		ops.each(function(i, v){
+			if(op_txt === v.innerText){
+				li.remove();
+				v.remove();
+				console.log('Removed '+op_txt);
+			}
+		});
+		// console.log(op, sel);
+	}
+}
+
+/**
+ * Helpers
+ * @param  {[type]} len [description]
+ * @return {[type]}     [description]
+ */
+function make_id(len) {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for (var i = 0; i < len; i++)
+	text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
+}
+
+function slugify(text){
+	return text.toString().toLowerCase()
+		.replace(/\s+/g, '_')           // Replace spaces with -
+		.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+		.replace(/\-\-+/g, '_')         // Replace multiple - with single -
+		.replace(/^-+/, '')             // Trim - from start of text
+		.replace(/-+$/, '');            // Trim - from end of text
+}
+
+
+// Actions
+__generate_btn.on('click', generate_form_data);
+__html_btn.on('click', switch_view);
+
+__col_btn.on('click', make_column);
+__mr_btn.on('click', add_main_row);
+
+__doc.on('click', '.inner_row_btn', add_inner_row);
+__doc.on('change', '.col_btn', add_column);
+
+__doc.on('change', '.form_field_select', on_select_field);
+__doc.on('keyup', '.in_field', add_field_title);
+__doc.on('click', '.add_select_option', add_options);
+__doc.on('click', '.delete', remove_option);
+__doc.on('click', '.add_desc_btn', heading_text);
+
+__doc.on('click', '.remove_btn', remove_action);
+__doc.on('click', '.minimize_btn', minimize_action);
+__doc.on('click', '.copy_btn', copy_action);
+
+
+
---
+++ js/template.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Main row template
+var main_row = "<div class=\"row cb_layer_box\">\n\t<div class=\"col-sm-12 removable\">\n\t\t<span class=\"on_minimize\"></span>\n\t\t<span class=\"pull-right win_control\">\n\t\t\t<span class=\"cb_move_btn move_bar\" style=\"color: #222\" title=\"Move\">\n\t\t\t\t<i class=\"fa fa-arrows\"></i>\n\t\t\t</span>\n\t\t\t<span class=\"copy_btn\" data-copy-txt=\"main\" title=\"Copy\">\n\t\t\t\t<i class=\"fa fa-copy\"></i>\n\t\t\t</span>\n\t\t\t<span class=\"minimize_btn\" data-minimize-txt=\"main\" title=\"Minimize\">\n\t\t\t\t<i class=\"fa fa-minus\"></i>\n\t\t\t</span>\n\t\t\t<span class=\"remove_btn\" data-remove-txt=\"main\" title=\"Remove\">\n\t\t\t\t<i class=\"fa fa-close\"></i>\n\t\t\t</span>\n\t\t</span>\n\t</div>\n\n\t<div class=\"col-sm-12 minimize_box\">\n\t\t<!-- Drag bar -->\n\t\t<span class=\"side_move_bar cb_move_btn removable\">\n\t\t\t<i class=\"fa fa-bars\"></i>\n\t\t</span>\n\t\t\n\t\t<div class=\"cb_display_box no-drag\">\n\n\t\t\t<div class=\"no-drag cb_inner_layer_box\">\n\t\t\t\t<div class=\"content-edit\">\n\t\t\t\t\t<h2 class=\"group_title\" contenteditable=\"\">Group title</h2>\n\t\t\t\t\t<div class=\"desc_box\">\n\t\t\t\t\t\t<span class=\"add_desc_btn removable\">\n\t\t\t\t\t\t\t<i class=\"fa fa-pencil\"></i> Add Description\n\t\t\t\t\t\t</span>\n\t\t\t\t\t\t<span class=\"add_desc_box\">\n\t\t\t\t\t\t\t<span contenteditable=\"\">-- Description text here</span>\n\t\t\t\t\t\t\t<span class=\"remove_btn\" data-remove-txt=\"heading\">\n\t\t\t\t\t\t\t\t<i class=\"fa fa-close\"></i>\n\t\t\t\t\t\t\t</span>\n\t\t\t\t\t\t</span>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\n\t\t\t\t<div class=\"inner_row_append_box\"></div>\n\t\t\t</div>\n\n\t\t\t<!-- row btn -->\n\t\t\t<div class=\"removable\">\n\t\t\t\t<a href=\"#\" class=\"btn btn-info btn_round inner_row_btn\">\n\t\t\t\t\t<i class=\"fa fa-plus\"></i> Add Row\n\t\t\t\t</a>\n\t\t\t</div><!-- end row btn -->\n\t\t</div>\n\t</div>\n</div>";
+
+
+// Inner row template
+var inner_row = "<div class=\"no-drag inner_col__box\">\t\t\t\t\t\t\t\t\t\n\t<!-- Control btn -->\n\t<div class=\"handle_bar removable\">\n\t\t<span class=\"cb_layer_control\">\n\n\t\t\t<select class=\"col_btn\">\n\t\t\t\t<option selected=\"\" disabled=\"\">-- Select Column --</option>\n\t\t\t\t<option value=\"1\">Full Column</option>\n\t\t\t\t<option value=\"2\">2 Column</option>\n\t\t\t\t<option value=\"3\">3 Column</option>\n\t\t\t\t<option value=\"4\">4 Column</option>\n\t\t\t</select>\n\t\t</span>\n\n\t\t<span class=\"pull-right win_control\">\n\t\t\t<span href=\"#\" class=\"copy_btn\" data-copy-txt=\"inner_row\" title=\"Copy\">\n\t\t\t\t<i class=\"fa fa-copy\"></i>\n\t\t\t</span> \n\t\t\t<span class=\"minimize_btn\" data-minimize-txt=\"inner_row\" title=\"Minimize\">\n\t\t\t\t<i class=\"fa fa-minus\"></i>\n\t\t\t</span> \n\t\t\t<span class=\"remove_btn\" data-remove-txt=\"inner_row\" title=\"Remove\">\n\t\t\t\t<i class=\"fa fa-times\"></i>\n\t\t\t</span>\n\t\t</span>\n\t</div><!-- end Control btn -->\n\t\n\t<!-- Form input box -->\n\t<div class=\"row form_row_box\">\n\t</div><!-- Form input box -->\n</div>";
+
+
+// Select field
+var form_select_bar = "<div class=\"field_box\">\n\t<select class=\"form-control form_field_select removable\">\n\t\t<option selected=\"\" disabled=\"\">-- Select Field --</option>\n\t\t<option value=\"input\">Input</option>\n\t\t<option value=\"textarea\">Textarea</option>\n\t\t<option value=\"select\">Select</option>\n\t\t<option value=\"checkbox\">Checkbox</option>\n\t\t<option value=\"radio\">Radio button</option>\n\t</select>\n\n\t<div class=\"form_field_box\"></div>\n</div>";
+
+// In fields editables
+var in_field_edit = "<span class=\"pull-right removable\">\n\t<span class=\"in_field_edit\" style=\"color: #aaa\">\n\t\t<i class=\"fa fa-pencil\"></i>\n\t</span>\n\t<span class=\"remove_btn\" data-remove-txt=\"field\">\n\t\t<i class=\"fa fa-close\"></i>\n\t</span>\n</span>";
+
+
+// Select Temp
+function add_option_temp(a){var b='';return'select'===a&&(b+='<ul class="select_list removable"></ul>'),('checkbox'===a||'radio'===a)&&(b+='<div class="select_list"></div>'),b+='\n\t\t\t<div class="input-group select_add_list removable">\n\t\t\t   <input type="text" class="form-control"  placeholder="Add Option">\n\t\t\t   <span class="input-group-addon add_select_option" title="Add" data-option-text="'+a+'">\n\t\t\t   \t<i class="fa fa-plus"></i>\n\t\t\t   </span>\n\t\t\t</div>\n\t\t',b}
+
+
+
